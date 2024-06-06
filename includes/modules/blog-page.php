c32403327045990fb5d499af43bae4d0e304d827
EJ Perez <ej.perez@stok.se>
2024-06-06 18:23:59 +0800

    feat: meta tags

--- includes/modules/blog-page.php
+++ includes/modules/blog-page.php
@@ -6,6 +6,11 @@ class ModuleBlogPage extends Module
 	{
 		$this->component = 'blogpage';
 		parent::__construct($data, $post, $translations);
+		$image = '';
+
+		if (has_post_thumbnail($post)) {
+			$image = $this->mapImage(get_post_thumbnail_id($post))['filename'];
+		}
 
 		$this->localizeField('title', function ($post) {
 			return $post->post_title;
@@ -31,6 +36,22 @@ class ModuleBlogPage extends Module
 
 		$this->localizeField('author', function ($post) {
 			return get_field('author', $post->ID);
+		});
+
+		$this->localizeField('Meta', function ($post) use ($image) {
+			$preamble = get_field('preamble', $post->ID);
+
+			return [
+				'title' => $post->post_title,
+				'plugin' => 'seo_metatags',
+				'og_image' => $image,
+				'og_title' => $post->post_title,
+				'description' => $preamble,
+				'twitter_image' => $image,
+				'twitter_title' => $post->post_title,
+				'og_description' => $preamble,
+				'twitter_description' => $preamble
+			];
 		});
 
 		$this->block['thumbnail'] = has_post_thumbnail($post->ID) ? $this->mapImage(get_post_thumbnail_id($post->ID)) : [];
